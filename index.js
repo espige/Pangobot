8e4bd885d2cf89614ff52c298f73073d62a9a02e
espige <ellie.spigelman@gmail.com>
2021-08-20 15:29:09 -0400

    working slash commands

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { Client, Intents, Message, Collection } = require('discord.js');
+const fs = require('fs');
 const { token } = require('./config.json');
-const { Client, Intents, Message } = require('discord.js');
 
 const intents = [
     Intents.FLAGS.GUILDS,
@@ -10,7 +11,14 @@ const intents = [
 // create a discord client
 const client = new Client({ intents });
 
-const prefix = '!';
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -18,24 +26,42 @@ client.once('ready', () => {
     console.log('Ready!');
 })
 
-client.on('messageCreate', async (msg) => {
-    // console.log(msg.content);
-    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
-    const args = msg.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+	const command = client.commands.get(interaction.commandName);
 
-    if (command === 'ping') {
-        // await msg.channel.send('Pong!');
-        await msg.reply('Pong!');
-    } else if (command === 'beep') {
-        // await msg.channel.send('Boop!');
-        await msg.reply('Boop!');
-    } else if (command === 'server') {
-        // await msg.channel.send(`This server's name is ${msg.guild.name}`);
-        await msg.reply(`This server's name is ${msg.guild.name}`);
-    }
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
+
+// TODO: use message code later for profanity filter
+
+// const prefix = '!';
+// client.on('messageCreate', async (msg) => {
+//     // console.log(msg.content);
+//     if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+//     const args = msg.content.slice(prefix.length).split(/ +/);
+//     const command = args.shift().toLowerCase();
+
+//     if (command === 'ping') {
+//         // await msg.channel.send('Pong!');
+//         await msg.reply('Pong!');
+//     } else if (command === 'beep') {
+//         // await msg.channel.send('Boop!');
+//         await msg.reply('Boop!');
+//     } else if (command === 'server') {
+//         // await msg.channel.send(`This server's name is ${msg.guild.name}`);
+//         await msg.reply(`This server's name is ${msg.guild.name} and id is ${msg.guild.id}`);
+//     }
+// });
 
 // login to discord with your app's token
 client.login(token)
