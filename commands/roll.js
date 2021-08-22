52318d33b9356d47c36fbd220fd7677f21853caf
espige <ellie.spigelman@gmail.com>
2021-08-22 16:10:04 -0400

    working dice roll slash command

--- commands/roll.js
+++ commands/roll.js
@@ -1,18 +1,50 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 const {randBetween} = require('../helperFunctions');
 
 const data = new SlashCommandBuilder()
     .setName('roll')
     .setDescription('Roll dice')
-    .addIntegerOption(option => option.setName('amount').setDescription('How many dice?'))
-    .addIntegerOption(option => option.setName('sides').setDescription('Number of sides'));
+    .addStringOption(option =>
+        option.setName('dice')
+            .setDescription('List what dice you want rolled ex. d20 or 4d6')
+            .setRequired(true)
+    );
 
 const execute = async (interaction) => {
-    const amount = interaction.options.getInteger('amount');
-    const sides = interaction.options.getInteger('sides');
-    console.log(amount);
-    console.log(sides);
-    await interaction.reply(randBetween(1, sides));
+    const diceString = interaction.options.getString('dice');
+    const regex = new RegExp('^(\\d)*(d|D){1}(\\d)+$');
+    const isMatch = regex.test(diceString);
+
+    if (isMatch) {
+        let [numDice, numSides] = diceString.split('d');
+        numDice = numDice === '' ? 1 : parseInt(numDice);
+        numSides = parseInt(numSides);
+        if (numDice < 1 || numSides < 2) {
+            await interaction.reply({
+                content: 'Beep boop. Please enter a valid number of dice or sides', 
+                ephemeral: true,
+            });
+        } else {
+            let rolls = '';
+            let total = 0;
+            for (let i = 0; i < numDice; i++) {
+                const roll = randBetween(1, numSides);
+                rolls += `${roll}, `;
+                total += roll;
+            }
+            const results = new MessageEmbed()
+                .setTitle(`Rolled ${numDice}d${numSides}`)
+                .addField('Rolls', rolls.substring(0, rolls.length - 2));
+            if (numDice > 1) results.addField('Total', `${total}`);
+            await interaction.reply({ embeds: [results] });
+        }
+    } else {
+        await interaction.reply({
+            content: 'Beep boop. Please use the correct format ex. d20 or 4d6',
+            ephemeral: true,
+        });
+    }
 }
 
 module.exports = {
