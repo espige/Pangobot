7c02e6187617a9eb6f0f6dba1c07e845e84c5810
espige <ellie.spigelman@gmail.com>
2021-08-27 11:49:59 -0400

    added different versions of deploy for different environments

--- deployCommands.js
+++ deployCommands.js
@@ -2,8 +2,9 @@
 
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildIdTest, token } = require('./config.json');
 const fs = require('fs');
+const yargs = require('yargs');
+const config = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -13,14 +14,30 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
+let token;
+let clientId;
+let guildId;
+if (yargs.argv.env === 'DEV') {
+	token = config.tokenDev;
+	clientId = config.clientIdDev;
+	guildId = config.guildIdBotTestServer;
+} else if (yargs.argv.env === 'PROD') {
+	token = config.tokenProd;
+	clientId = config.clientIdProd;
+	guildId = config.guildIdSocialPangolin;
+} else {
+	throw new Error('Argument must be a valid environment');
+}
+
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
+	console.log(yargs.argv);
 	try {
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildIdTest),
+			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
 
